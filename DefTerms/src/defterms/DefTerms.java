116d1bb9ce7675538e14a128c34818ad89dc41f1
conquerYa <andrew286014@gmail.com>
2014-11-23 16:08:30 +0200

    defterms

---
+++ DefTerms/src/defterms/DefTerms.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package defterms;
+
+/**
+ *
+ * @author Andrew
+ */
+public class DefTerms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MForm mf= new MForm();
+        mf.setVisible(true);
+        mf.setLocation(100,100);
+    }
+    
+}
